c269044d7bacd263b0837c2342bea39b54c5b808
Yakimax <sv1099912@gmail.com>
2023-10-19 18:45:33 +0530

    add

--- Day1/FactorialOfN/Main.java
+++ Day1/FactorialOfN/Main.java
@@ -1,6 +1,13 @@
 package Day1.FactorialOfN ;
 public class Main {
+    static int fibonacci(int idx){ 
+        if( idx <= 2 ){
+            return 1 ;
+        }
+        return fibonacci(idx-1)+fibonacci(idx-2)  ;
+    }
     public static void main(String[] args) {
-        
+        int val = fibonacci(5) ;
+        System.out.println(val) ;
     }
 }
