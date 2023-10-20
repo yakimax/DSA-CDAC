13e8c5ee71000603bef9caec4c5a5e9c39ea0a63
Yakimax <sv1099912@gmail.com>
2023-10-20 09:55:31 +0530

    add

--- Day1/TowerOfHanoi/Main.java
+++ Day1/TowerOfHanoi/Main.java
@@ -2,14 +2,17 @@ package Day1.TowerOfHanoi ;
 
 public class Main{
 
-    public static void toi(int n,char src,char aux,char dest){
-        if(n==1){
+    public static void toi(int n,char src,char dest,char aux){
+        if(n==0){
             return  ;
         }
-        System.out.println(n,);
+        toi(n-1,src,aux,dest) ;
+        System.out.println("Moving ring "+n+" From rod "+src+" to rod "+dest) ;
+        toi(n-1,aux,dest,src) ;
+        // System.out.println("Moving ring "+n+" From rod"+src+"to rod"+dest) ;
     }
 
     public static void main( String[] args ) {
-        toi(3,'a','b','c');
+        toi(3,'A','C','B') ;
     }
 }
