b566481058c8115b0dbb58f2d6e712d94cb22299
Yakimax <sv1099912@gmail.com>
2023-10-20 10:32:55 +0530

    add

---
+++ Day2/Palindrome/Main.java
@@ -0,0 +1,26 @@
+package Day2.Palindrome;
+
+import java.util.*;
+
+
+
+public class Main {
+
+    public static boolean palindrome(String str ){
+        if ( str.length() <= 1 ) {
+            return true ;
+        }
+        if ( str.charAt(0) != str.charAt(str.length()-1) ) {
+            return false ;
+        }
+        boolean a = palindrome(str.substring(1,str.length()-1) ) ;
+        
+        return a ;
+    } 
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in) ;
+        String s = scn.nextLine() ;
+        System.out.println( palindrome(s) ) ;
+    }
+}
