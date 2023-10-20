e9d155d60452a82474d7b2dd94e605ca5d865952
Yakimax <sv1099912@gmail.com>
2023-10-20 09:02:54 +0530

    add

--- Day1/TowerOfHanoi/Main.java
+++ Day1/TowerOfHanoi/Main.java
@@ -1,7 +1,15 @@
 package Day1.TowerOfHanoi ;
 
 public class Main{
+
+    public static void toi(int n,char src,char aux,char dest){
+        if(n==1){
+            return  ;
+        }
+        System.out.println(n,);
+    }
+
     public static void main( String[] args ) {
-        
+        toi(3,'a','b','c');
     }
 }
