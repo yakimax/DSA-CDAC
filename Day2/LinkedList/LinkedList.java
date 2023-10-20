20fd9dd47bd28c44cff20e177a4c7b5bdfb73caa
Yakimax <sv1099912@gmail.com>
2023-10-20 15:35:23 +0530

    add

---
+++ Day2/LinkedList/LinkedList.java
@@ -0,0 +1,14 @@
+package Day2.LinkedList;
+
+class Node {
+    int data ;
+    Node node ;
+    void setData( int val ) {
+        this.data = val ;
+    }
+    
+}
+
+public class LinkedList {
+    
+}
