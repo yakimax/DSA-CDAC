8ae8797f1e2a82f6596a51bdeadcd86ec8d46cf4
Yakimax <sv1099912@gmail.com>
2023-10-21 17:33:09 +0530

    add

--- Day3/StaticClass/Main.java
+++ Day3/StaticClass/Main.java
@@ -1,14 +1,14 @@
 package Day3.StaticClass ;
 
-class First{
-    static class Node{
+class First {
+    static class Node {
         int a ;
         Node node ;
     }
 }
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main ( String[] args ) {
         First f = new First() ;
         
     }
