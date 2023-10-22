55f5d7f535c10fbf81ce30253aaee5d1f073184d
Yakimax <sv1099912@gmail.com>
2023-10-22 14:30:28 +0530

    add

--- Day4/DoublyLinkedList/DoublyLinkedList.java
+++ Day4/DoublyLinkedList/DoublyLinkedList.java
@@ -14,10 +14,49 @@ class Node {
 
 
 public class DoublyLinkedList {
-    Node head ;
-    Node tail ;
+    Node head = null ;
+    Node tail = null ;
     int size ;
-    void  addFirst ( int val ) {
-        
+ 
+    void  addFirst ( int val ) { 
+        Node node = new Node(val) ;
+            if ( head == null ) { 
+                head = tail = node ; 
+            } else {
+                node.next = head ;
+                head.prev = node ;
+                head = node ;
+            }
+    }
+
+    void addLast ( int val ) {
+        Node node = new Node() ;
+        if (tail == null) {
+            addFirst(val) ;
+        }else {
+            tail.next = node ;
+            node.prev = tail ;
+            tail = node ;
+        }
+    }
+
+    void addNodeAt ( int idx , int val ) {
+        Node node = new Node(val) ;
+        if( head == null || idx == 0 ) {
+            addFirst(val) ;
+        }else if (idx == size) {
+             addLast(val) ;
+        }else {
+            Node temp = head ;
+            while( idx > 1 ) {
+                idx-- ;
+                temp = temp.next ;
+            }
+            Node after = temp.next ;
+            temp.next = node ;
+            node.prev = temp ;
+            node.next = after ;
+            after.prev = node ;
+        }
     }
 }
