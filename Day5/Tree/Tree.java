7cf606b50f7d9941cb7828a49147d09f75422edf
Yakimax <sv1099912@gmail.com>
2023-10-24 01:58:05 +0530

    add

--- Day5/Tree/Tree.java
+++ Day5/Tree/Tree.java
@@ -34,17 +34,27 @@ public class Tree {
     Tree ( ) {
         this.root = new Node() ;
     }
-    void constructTree(int arr) {
-        Stack< Pair > st = new Stack() ;
+    Tree (int data) {
+        this.root = new Node(data) ;
+    }
+    void constructTree(int arr[]) {
+        Stack< Pair > st = new Stack<>() ;
+        int i = 0 ;
         st.push ( new Pair(root,0) ) ;
         while ( st.size() > 0 ) {
             Pair p = st.pop() ;
             if ( p.stage == 0 ) {
-                
+                p.stage++ ;
+                Node temp = new Node( arr[i++] ) ;
+                p.node.left = temp ;
+                st.push(p) ;
             } else if ( p.stage == 1 ) {
-
+                p.stage++ ;
+                Node temp = new Node( arr[i++] ) ;
+                p.node.right = temp ;
+                st.push(p) ;
             } else {
-
+                
             }
         }
     }
