bbe09888d45880a0f7b68320822fb1ae7988bed8
Yakimax <sv1099912@gmail.com>
2023-10-24 01:38:52 +0530

    add

--- Day5/Tree/Tree.java
+++ Day5/Tree/Tree.java
@@ -1,23 +1,33 @@
 // package Day5.Tree ;
+
+
 import java.util.* ;
 class Node {
     int data ;
-    Node left,right ;
+    Node left , right ;
     Node ( ) {
         this.left = null ;
         this.right = null ;
     }
-    Node ( Node left ,Node right ) {
+    Node ( Node left , Node right ) {
         this.left = left ;
         this.right = right ;
     }
-    Node (int val) {
+    Node ( int val ) {
         this.data = val ;
         this.left = null ;
         this.right = null ;
     }
 }
 
+class Pair {
+    int stage ;
+    Node node ;
+    Pair ( Node node , int stage ) {
+        this.stage = stage ;
+        this.node = node ;
+    }
+}
 
 public class Tree {
     Node root ;
@@ -25,7 +35,17 @@ public class Tree {
         this.root = new Node() ;
     }
     void constructTree(int arr) {
-        Stack  st = new Stack() ;
-        st.push(root) ;
+        Stack< Pair > st = new Stack() ;
+        st.push ( new Pair(root,0) ) ;
+        while ( st.size() > 0 ) {
+            Pair p = st.pop() ;
+            if ( p.stage == 0 ) {
+                
+            } else if ( p.stage == 1 ) {
+
+            } else {
+
+            }
+        }
     }
 }
