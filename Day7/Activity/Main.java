aeb450a83acc2efca648d6f73ee2dffe75c9328f
Yakimax <sv1099912@gmail.com>
2023-10-25 18:34:49 +0530

    add

---
+++ Day7/Activity/Main.java
@@ -0,0 +1,19 @@
+
+package Day7.Activity ;
+
+
+class Employee{
+    int EmpId ;
+    String EmpName ;
+    float EmpSalary ;
+    String EmpDept;
+    int EmpAge ;
+    String EmpDesig ;
+
+}
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }    
+}
