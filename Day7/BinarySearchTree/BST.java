80fda69bb4eff014c174fde279affe9f6c5b3f30
Yakimax <sv1099912@gmail.com>
2023-10-26 00:29:19 +0530

    add

---
+++ Day7/BinarySearchTree/BST.java
@@ -0,0 +1,35 @@
+package Day7.BinarySearchTree ;
+
+class Node {
+    Node left , right ;
+    int data ;
+    Node ( ) {
+        this.left = null ;
+        this.right = null ;
+    }
+    Node ( int data ) {
+        this.data = data ;
+    }
+}
+
+public class BST {
+    Node root ;
+    BST ( ){
+        root = new Node ( ) ;
+    }
+
+    Node insertdata(Node root, int key)
+	{
+		if(root == null)
+		{
+			root = new Node(key) ;
+			return root ;
+		}
+		if(key <= root.data){
+			root.left = insertdata(root.left, key) ;
+        }else{
+			root.right = insertdata(root.right, key) ;
+        }
+        return root ;
+	}
+}
