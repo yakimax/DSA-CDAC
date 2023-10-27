485accac5588931d78e28e580140c5c7354b0937
Yakimax <sv1099912@gmail.com>
2023-10-27 17:23:28 +0530

    add

--- Day9/DemonstrateQueue/Main.java
+++ Day9/DemonstrateQueue/Main.java
@@ -4,20 +4,10 @@ import Day9.DynamicQueue.* ;
 public class Main {
     public static void main(String[] args) {
         Queue q = new Queue() ;
-        // q.enqueue(10) ;
-        // q.enqueue(20) ;
-        // q.enqueue(30) ;
-        // q.enqueue(40) ;
-        // // System.out.println ( q.front.data ) ;
-        // System.out.println ( q.dequeue() ) ;
-        // System.out.println ( q.dequeue() ) ;
-        // System.out.println ( q.dequeue() ) ;
-        // System.out.println ( q.dequeue() ) ;
- 
-        for(int i = 0 ; i < 10 ; i++){
-            q.enqueue(i);
+        for ( int i = 0 ; i < 10 ; i++ ) {
+            q.enqueue(i) ;
         }
-        while(!q.isEmpty()){
+        while( !q.isEmpty() ) {
             System.out.println(q.dequeue( )) ;
         }
     }
