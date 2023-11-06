eedf0d16562eed799955164f9750e30d8cbc5376
Yakimax <sv1099912@gmail.com>
2023-11-06 09:17:38 +0530

    add

---
+++ Day10/Main.java
@@ -0,0 +1,13 @@
+package Day10 ;
+public class Main{
+    int a ;
+    int b ;
+    Main ( int a , int b ) {
+        this.a = 0 ;
+        this.b = 0 ;
+    }
+    public static void main(String[] args) {
+        Main m = new Main() ;
+        System.out.println(" ") ;
+    }
+}
